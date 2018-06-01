131d9f1d9530e9e140e357302ee50b8703f8d3d9
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-06-01 08:53:28 +0500

    Fix https://github.com/mubaidr/vue-fluent/issues/13

---
+++ src/docs/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // Project deployment base
+  // By default we assume your app will be deployed at the root of a domain,
+  // e.g. https://www.my-app.com/
+  // If your app is deployed at a sub-path, you will need to specify that
+  // sub-path here. For example, if your app is deployed at
+  // https://www.foobar.com/my-app/
+  // then change this to '/my-app/'
+  baseUrl: '/vue-fluent/',
+
+  // where to put static assets (js/css/img/font/...)
+  // assetsDir: 'assets',
+
+  // configureWebpack: () => {},
+}
