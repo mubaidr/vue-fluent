5f40e6754e4b237bcde10d4938b0534eb71de4dd
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-10-01 11:18:12 +0500

    update docs

---
+++ docs/precache-manifest.d5c1b7ee1fde3b08ffbe371d76fbba16.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c39a8a45df792adb54b794182b5dba2",
+    "url": "/vue-fluent/fonts/fa-solid-900.9c39a8a4.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-fluent/robots.txt"
+  },
+  {
+    "revision": "9404b3cb62fa977e95ceb5b53044f192",
+    "url": "/vue-fluent/fonts/fa-brands-400.9404b3cb.woff2"
+  },
+  {
+    "revision": "a6c3bdfb93535fbf681c",
+    "url": "/vue-fluent/css/app.6644c869.css"
+  },
+  {
+    "revision": "79faaaef7c3ddc452324",
+    "url": "/vue-fluent/js/chunk-vendors.2ac44373.js"
+  },
+  {
+    "revision": "d685ed8c997e3f7e8f57",
+    "url": "/vue-fluent/css/demo.2cd6703a.css"
+  },
+  {
+    "revision": "d685ed8c997e3f7e8f57",
+    "url": "/vue-fluent/js/demo.86bc8a6a.js"
+  },
+  {
+    "revision": "6035fba646b5d4d4cf9a",
+    "url": "/vue-fluent/css/extras.2b3deb1b.css"
+  },
+  {
+    "revision": "6035fba646b5d4d4cf9a",
+    "url": "/vue-fluent/js/extras.e83d17b9.js"
+  },
+  {
+    "revision": "8c98619845ad2a91084e0b881e0671e4",
+    "url": "/vue-fluent/fonts/fa-regular-400.8c986198.woff"
+  },
+  {
+    "revision": "28ec6d38ccb96288be39293dae9ba767",
+    "url": "/vue-fluent/fonts/fa-regular-400.28ec6d38.woff2"
+  },
+  {
+    "revision": "e07d72d705d882694ab4a4efce9f7104",
+    "url": "/vue-fluent/fonts/fa-regular-400.e07d72d7.eot"
+  },
+  {
+    "revision": "0b697cf43612b2764c55b3ed9eae0934",
+    "url": "/vue-fluent/fonts/fa-regular-400.0b697cf4.ttf"
+  },
+  {
+    "revision": "a8f61abb8024bd0bcd2b",
+    "url": "/vue-fluent/js/api.41c000e9.js"
+  },
+  {
+    "revision": "a6c3bdfb93535fbf681c",
+    "url": "/vue-fluent/js/app.3eb88110.js"
+  },
+  {
+    "revision": "4ff89f9329d4a4c28f58dd5ef7f08651",
+    "url": "/vue-fluent/fonts/fa-solid-900.4ff89f93.woff"
+  },
+  {
+    "revision": "cc6aff5040868e4b27fdcfdaa4647746",
+    "url": "/vue-fluent/fonts/fa-brands-400.cc6aff50.woff"
+  },
+  {
+    "revision": "e2a7835b2b25aab252c3506cfdfd6507",
+    "url": "/vue-fluent/fonts/fa-brands-400.e2a7835b.eot"
+  },
+  {
+    "revision": "c601db56ffa80d05739b42d9c9788c31",
+    "url": "/vue-fluent/fonts/fa-brands-400.c601db56.ttf"
+  },
+  {
+    "revision": "576b41bc58d121b9d4e5df578945461c",
+    "url": "/vue-fluent/img/fa-regular-400.576b41bc.svg"
+  },
+  {
+    "revision": "24f9359f2b036d41c1aa739942f86024",
+    "url": "/vue-fluent/fonts/fa-solid-900.24f9359f.eot"
+  },
+  {
+    "revision": "af4698a4a8ea6baa01c4c8bc3969f8e2",
+    "url": "/vue-fluent/fonts/fa-solid-900.af4698a4.ttf"
+  },
+  {
+    "revision": "11c05d473f66db7ce479bfefc6d2bfc0",
+    "url": "/vue-fluent/img/fa-solid-900.11c05d47.svg"
+  },
+  {
+    "revision": "e29e9563cff9c09a6ac50596ba2515f4",
+    "url": "/vue-fluent/img/fa-brands-400.e29e9563.svg"
+  },
+  {
+    "revision": "0f49fd01a29cbda08cd7246d23616525",
+    "url": "/vue-fluent/index.html"
+  },
+  {
+    "revision": "a8f61abb8024bd0bcd2b",
+    "url": "/vue-fluent/css/api.3b4c83a9.css"
+  }
+];
